8f36176cf00ec0de4fa8a6f74a936972b112cf25
ESI-8Bitman <esi.inc.company@gmail.com>
2024-09-08 10:38:54 +0330

    Add files via upload

---
+++ Joystick.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Joystick : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    Transform pointer;
+    Vector2 centerPos,beginPos,dragPos,dir;
+    float r;
+
+    void Start(){
+        pointer = transform.GetChild(0);
+        r = GetComponent<RectTransform>().sizeDelta.x / 2;
+        centerPos = pointer.position;
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        beginPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragPos = eventData.position;
+        dir = dragPos - beginPos;
+        pointer.position = Vector2.Distance(dragPos, beginPos) > r ? (centerPos + dir.normalized * r) : (centerPos + dir);
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        dragPos = Vector2.zero;
+        beginPos = Vector2.zero;
+        pointer.position = centerPos;
+    }
+
+}
